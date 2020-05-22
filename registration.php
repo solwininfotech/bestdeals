13166397dc8054abfa5b3300f769b41ef9e19222
solwininfotech <info@solwininfotech.com>
2020-05-22 17:35:32 +0530

    initial commit

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Best Deals Product Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_BestDeals
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/s
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_BestDeals',
+    __DIR__
+);
